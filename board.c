0145b1bc675d7f480ddc159bb67866c7a769ded7
yg-cpu <anirudhyamunang@gmail.com>
2021-07-20 16:29:57 +0200

    added svg assets

--- board.c
+++ board.c
@@ -1,6 +1,13 @@
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 #include <cairo/cairo.h>
+#include <cairo/cairo-svg.h>
+
+enum Board
+{
+  space = 0, king = 1, pawn = 2, knight = 3, bishop = 4, rook = 5, queen = 6,
+  white = 8, black = 16
+};
 
 void DrawBoard(GtkWidget* widget, GdkEventExpose* eev, gpointer data)
 {
@@ -17,7 +24,15 @@ void DrawBoard(GtkWidget* widget, GdkEventExpose* eev, gpointer data)
 	if ((i + j) % 2 == 0) cairo_set_source_rgb(cr, 1, 1, 1);
 	else                  cairo_set_source_rgb(cr, 0, 0, 0);
       }
-  
+
   cairo_stroke(cr);  
+  
+  int squares[64];
+  squares[0] = white | queen;
+  // TODO: SVG: https://zetcode.com/gfx/cairo/cairobackends/
+  cairo_surface_t* image = cairo_svg_surface_create("data/assets/b_queen.png", 100, 100);
+  cairo_set_source_surface(cr, image, 10, 10);
+  cairo_paint(cr);
+  
   cairo_destroy(cr);
 }
