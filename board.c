c10e3d2ef891a5a83b1ffa8310b8813af6703aac
yg-cpu <anirudhyamunang@gmail.com>
2021-07-21 11:57:47 +0200

    added drawing from png

--- board.c
+++ board.c
@@ -11,7 +11,7 @@ enum Board
 
 void DrawBoard(GtkWidget* widget, GdkEventExpose* eev, gpointer data)
 {
-  cairo_t* cr = gdk_cairo_create (widget->window);
+  cairo_t* cr = gdk_cairo_create(widget->window);
   
   for (unsigned i = 0; i < 8; i++)
     for (unsigned j = 0; j < 8; j++)
@@ -24,15 +24,16 @@ void DrawBoard(GtkWidget* widget, GdkEventExpose* eev, gpointer data)
 	if ((i + j) % 2 == 0) cairo_set_source_rgb(cr, 1, 1, 1);
 	else                  cairo_set_source_rgb(cr, 0, 0, 0);
       }
-
+  
   cairo_stroke(cr);  
   
   int squares[64];
   squares[0] = white | queen;
-  // TODO: SVG: https://zetcode.com/gfx/cairo/cairobackends/
-  cairo_surface_t* image = cairo_svg_surface_create("data/assets/b_queen.png", 100, 100);
-  cairo_set_source_surface(cr, image, 10, 10);
+  cairo_surface_t* surface = cairo_image_surface_create_from_png("data/assets/w_queen.png");
+  cairo_set_source_surface(cr, surface, 80, 80);
+  
   cairo_paint(cr);
   
+  cairo_surface_destroy(surface);
   cairo_destroy(cr);
 }
