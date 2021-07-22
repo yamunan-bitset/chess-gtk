bc91978bc2988ca9bfd2c80e4eab71682b49be9f
yg-cpu <anirudhyamunang@gmail.com>
2021-07-22 13:02:20 +0200

    changed colours

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC      = cc
-CFLAGS  = -Wall -Wextra $(shell pkg-config --cflags gtk+-2.0)
+CFLAGS  = $(shell pkg-config --cflags gtk+-2.0)
 LDFLAGS = $(shell pkg-config --libs gtk+-2.0)
 OBJS    = main.o board.o
 OUT     = chess
--- board.c
+++ board.c
@@ -22,7 +22,7 @@ void DrawBoard(GtkWidget* widget, GdkEventExpose* eev, gpointer data)
 	cairo_rectangle(cr, i*100, j*100, 100, 100);
 	cairo_restore(cr);
 	if ((i + j) % 2 == 0) cairo_set_source_rgb(cr, 1, 1, 1);
-	else                  cairo_set_source_rgb(cr, 0, 0, 0);
+	else                  cairo_set_source_rgb(cr, 0.36470588235294116f, 0.5764705882352941f, 0.7333333333333333f);
       }
   
   cairo_stroke(cr);  
